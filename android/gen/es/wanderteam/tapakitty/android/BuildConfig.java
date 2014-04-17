ad955a055490e85f0c3a51ce1d188aad30d46235
ryuzaki <ojizaki@gmail.com>
2014-04-17 13:32:57 +0200

    initial import

---
+++ android/gen/es/wanderteam/tapakitty/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package es.wanderteam.tapakitty.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ core/src/es/wanderteam/tapakitty/CLASEPRUEBA.java
@@ -0,0 +1,5 @@
+package es.wanderteam.tapakitty;
+
+public class CLASEPRUEBA {
+
+}
